3d04afeb0b7a80a9f1d75caf649ba693dabd838a
MetalNeox <metalneox@gmail.com>
2025-05-14 11:51:27 +0000

    Update

---
+++ README.md
@@ -0,0 +1,10 @@
+![screenshot](https://raw.githubusercontent.com/metalneox/dotfiles/master/Screenshots/example.png)␍
+␍
+#Refactory DOTFILE & Migration HyprLand␍
+␍
+### Todo List␍
+  - [x]  Installation script (Alpha Test)␍
+  - [ ]  WayBar or alternative␍
+  - [ ]  Notification␍
+  - [ ]  WallPaper Animated␍
+  - [ ]  Lock Screen
---
+++ install.sh
@@ -0,0 +1,17 @@
+## Install Normal Packages
+pacman -S --needed --noconfirm - <packages
+
+#Script install yay
+
+## Install 3rd package
+yay -S --needed --noconfirm - <packages.aur
+
+## Configuration
+
+#git clone https://github.com/metalneox/dotfiles.git
+
+##Clear and install dotfiles
+#rm -rf ~/.*
+#cp -rT dotfiles ~/.
+#rm -rf dotfiles
+#rm ~/install.sh
