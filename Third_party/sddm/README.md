105897c2abe443d2bcded773444c921b697f204f
MetalNeox <metalneox@gmail.com>
2026-03-05 23:31:16 +0100

    Add SDDM Theme

---
+++ Third_party/sddm/README.md
@@ -0,0 +1,16 @@
+## SDDM Requirement
+
+## TODO: Check if need all this library
+
+- sddm
+- qt5-graphicaleffects
+- qt5-multimedia
+- qt5-quickcontrols
+- qt5-quickcontrols2
+- qt5-svg
+- gst-libav
+- gst-plugins-good
+- gst-plugins-bad
+- gst-plugins-ugly
+- fzf
+- ttf-jetbrains-mono-nerd"
--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ sudo pacman -S --needed --noconfirm - <packages
 #Script install yay
 
 ## Install 3rd package
-yay -S --needed --noconfirm - <packages.aur
+#yay -S --needed --noconfirm - <packages.aur
 
 ## Configuration
 
@@ -15,3 +15,24 @@ yay -S --needed --noconfirm - <packages.aur
 #cp -rT dotfiles ~/.
 #rm -rf dotfiles
 #rm ~/install.sh
+
+## SDDM Login
+
+THEME="cyberpunk"
+CONF_FILE="/etc/sddm.conf.d/theme.conf"
+
+sudo cp -r ./Third_party/sddm/* /usr/share/sddm/themes
+yay -S sddm qt5-graphicaleffects qt5-multimedia qt5-quickcontrols qt5-quickcontrols2 qt5-svg gst-libav gst-plugins-good gst-plugins-bad gst-plugins-ugly fzf ttf-jetbrains-mono-nerd --noconfirm
+sudo mkdir -p /etc/sddm.conf.d
+
+if [ ! -f "$CONF_FILE" ]; then
+  #If file dont exist
+  echo -e "[Theme]\nCurrent=$THEME" | sudo tee "$CONF_FILE" >/dev/null
+else
+  #if file exist modify value
+  if grep -q "^Current=" "$CONF_FILE"; then
+    sudo sed -i "s/^Current=.*/Current=$THEME/" "$CONF_FILE"
+  else
+    echo "Current=$THEME" | sudo tee -a "$CONF_FILE" >/dev/null
+  fi
+fi
